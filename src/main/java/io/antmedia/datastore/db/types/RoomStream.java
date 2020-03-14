bea2f3565915feadcd8678270d360fd0a5bed3b2
Selim Emre <selimemretoy@gmail.com>
2020-03-14 20:18:55 +0300

    Refactor RoomStream class

--- src/main/java/io/antmedia/datastore/db/types/RoomStream.java
+++ src/main/java/io/antmedia/datastore/db/types/RoomStream.java
@@ -1,9 +1,15 @@
 package io.antmedia.datastore.db.types;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(value="RoomStream", description="The Room Stream class")
 public class RoomStream {
 	
+	@ApiModelProperty(value = "the ID of Stream")
 	public  String streamId;
-
+	
+	@ApiModelProperty(value = "the IP of Stream Origin")
 	public  String streamOriginIP;
 	
 	public String getStreamId() {
@@ -18,5 +24,4 @@ public class RoomStream {
 	public void setStreamOriginIP(String streamOriginIP) {
 		this.streamOriginIP = streamOriginIP;
 	}
-
 }
